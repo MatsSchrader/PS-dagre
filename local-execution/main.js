3880c26f0a2d6dec4ea471cab814ad81d7749c64
MatsSchrader <mats.schrader@uni-hamburg.de>
2025-05-26 14:42:14 +0200

    improved align, "r" direction causing problems still

--- local-execution/main.js
+++ local-execution/main.js
@@ -4,19 +4,20 @@ import * as v from "./graphVariants.js";
 
 // this includes exactly the target process
 let g1to2 = initGraph(v.variant1to2);
-dagre.layout(g1to2);
+//dagre.layout(g1to2);
+//visualizeGraph(g1to2, false);
 
 let g1to3 = initGraph(v.variant1to3);
-dagre.layout(g1to3);
-
+//dagre.layout(g1to3);
+//
 let g1to4 = initGraph(v.variant1to4);
-dagre.layout(g1to4, null, g1to3);
-
+// dagre.layout(g1to4, null, g1to3);
+//
 let g1to7 = initGraph(v.variant1to7);
 dagre.layout(g1to7, null, g1to4);
-
-dagre.layout(g1to7); //uses last layout as oldGraph
-dagre.layout(g1to7, null, null); //ignores oldGraph
+//
+// dagre.layout(g1to7); //uses last layout as oldGraph
+// dagre.layout(g1to7, null, null); //ignores oldGraph
 visualizeGraph(g1to7, false);
 
 //ToDo:
@@ -36,6 +37,7 @@ function initGraph(variant) {
     nodesep: 50,
     edgesep: 10,
     ranksep: 75,
+    //align: 'UL',
   });
 
   // Default to assigning a new object as a label for each new edge.
