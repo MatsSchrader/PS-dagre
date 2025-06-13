0884ccc2476cd85473f89114982e152d13094f1b
MatsSchrader <mats.schrader@uni-hamburg.de>
2025-06-13 14:33:49 +0200

    removed outdated attempts and TODOs

--- local-execution/main.js
+++ local-execution/main.js
@@ -18,12 +18,6 @@ dagre.layout(g1to7); //uses last layout as oldGraph
 // dagre.layout(g1to7, null, null); //ignores oldGraph
 visualizeGraph(g1to7, false);
 
-//ToDo:
-// 	- get more Graphs to test
-// 	- make it easier to load different Graphs/variants: look overleaf, custom jointjs directed layout
-// 	- zoom Meeting with Supervisor for more data and Show current results
-// 	- collect opinions on new layout
-
 
 function initGraph(variant) {
 // Create a new directed graph
